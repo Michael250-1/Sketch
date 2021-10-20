8897e77f60cb66a955fba977ec87f4c5960ca6f7
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-10-19 18:57:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,37 @@ function setup(){
 Canvas=createCanvas(300, 300);
 Canvas.center();
 background("white");
+Canvas.mouseReleased(classifyCanvas);
+synth=window.speechSynthesis;
 }
 function ClearCanvas(){
     background("white");
 }
+function preload(){
+classifier=ml5.imageClassifier("DoodleNet");
+}
+function draw(){
+    strokeWeight(5);
+    stroke(0);
+    if (mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
 
+    }
+}
+function classifyCanvas(){
+    classifier.classify(Canvas, gotResult);
+}
+function gotResult(error,results){
+if (error){
+    console.error(error);
+}
+else {
+    console.log(results);
+    document.getElementById("label").innerHTML="Label : " + results[0].label;
+    document.getElementById("confidence").innerHTML="Confidence : " + Math.round(results[0].confidence * 100) + "%";
+    utterthis=new speechSynthesisUtterance(results[0].label);
+    synth.speak(utterthis);
+    
+}
+}
 
